4d7954b94b5bc2e2ccc7041f0c5982e205c4f2b3
Guillaume Leroy <pro.guillaume.leroy@gmail.com>
2020-11-28 14:10:56 +0100

    :bug: docker: fix kafka image

--- docker/kafka/start-kafka.sh
+++ docker/kafka/start-kafka.sh
@@ -1,9 +1,14 @@
 #!/usr/bin/env sh
 
+set -e
+
 if [ -z "$ZOOKEEPER_CONNECT" ]; then
   echo Missing ZOOKEEPER_CONNECT environment variable
   exit 1
 fi
 
+IP=$(hostname --ip-address)
+
+sed -i "s/^#listeners=.*/listeners=PLAINTEXT:\/\/$IP:9092/g" config/server.properties
 sed -i "s/^zookeeper\.connect=.*/zookeeper.connect=$ZOOKEEPER_CONNECT/g" config/server.properties
 exec ./bin/kafka-server-start.sh config/server.properties
